18baf95e08b8a29247907d43b077f909be22d60d
patelgayatri <gayatripatel566@gmail.com>
2022-06-26 15:12:09 +1000

    firebase app distribution added

--- app/src/main/java/com/techand/shopus/ui/Splash.kt
+++ app/src/main/java/com/techand/shopus/ui/Splash.kt
@@ -3,7 +3,6 @@ package com.techand.shopus.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.currencyconvertor.Currency
 
 import com.techand.shopus.R
 import kotlinx.coroutines.GlobalScope
@@ -14,7 +13,6 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        var result = Currency.convert(2000, 1.5)
         val intent = Intent(this, MainActivity::class.java)
         GlobalScope.launch {
             delay(3000L)
